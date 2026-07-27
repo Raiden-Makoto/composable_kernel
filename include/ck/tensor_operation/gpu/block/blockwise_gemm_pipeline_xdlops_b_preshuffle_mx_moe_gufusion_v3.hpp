5773baef9efc4e34ff16eb8135ef640397dbde9b
Raiden-Makoto <Raiden-Makoto@users.noreply.github.com>
2026-07-27 07:47:28 -0700

    [GLM5.2] Specialize MFMA priority for M64 tiles

--- include/ck/tensor_operation/gpu/block/blockwise_gemm_pipeline_xdlops_b_preshuffle_mx_moe_gufusion_v3.hpp
+++ include/ck/tensor_operation/gpu/block/blockwise_gemm_pipeline_xdlops_b_preshuffle_mx_moe_gufusion_v3.hpp
@@ -655,6 +655,13 @@ struct BlockwiseGemmXdlops_pipeline_bpreshuffle_mx_moe_gufusion_v3<
                             constexpr index_t c_offset = c_thread_desc_.CalculateOffset(
                                 make_tuple(im_major, in_major, im_minor, in_minor, 0));
 
+                            if constexpr(MPerBlock == 64 && k0.value == 0 && n0.value == 0)
+                            {
+                                __builtin_amdgcn_sched_barrier(0);
+                                __builtin_amdgcn_s_setprio(1);
+                                __builtin_amdgcn_sched_barrier(0);
+                            }
+
                             // MFMA accumulation A * Gate
                             xdlops_gemm.template Run<ik_minor * MXdlPack + im_minor,
                                                      ik_minor * NXdlPack + in_minor>(
@@ -672,18 +679,14 @@ struct BlockwiseGemmXdlops_pipeline_bpreshuffle_mx_moe_gufusion_v3<
                                 b_thread_vec_up.template AsType<mfma_input_type_b>(),
                                 b_scale_thread_vec_up.template AsType<mfma_scale_input_type_b>(),
                                 c_thread_buf_up.GetVectorTypeReference(Number<c_offset>{}));
-
-                            if constexpr(k0.value == 0 && n0.value == 0)
-                            {
-                                __builtin_amdgcn_sched_barrier(0);
-                                __builtin_amdgcn_s_setprio(1);
-                                __builtin_amdgcn_sched_barrier(0);
-                            }
                         });
 
-                        __builtin_amdgcn_sched_barrier(0);
-                        __builtin_amdgcn_s_setprio(0);
-                        __builtin_amdgcn_sched_barrier(0);
+                        if constexpr(MPerBlock == 64)
+                        {
+                            __builtin_amdgcn_sched_barrier(0);
+                            __builtin_amdgcn_s_setprio(0);
+                            __builtin_amdgcn_sched_barrier(0);
+                        }
 
                         if constexpr(m0.value == SwitchM)
                         {
